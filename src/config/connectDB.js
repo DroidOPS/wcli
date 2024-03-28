b44face5bac404425a475c0593ed74bdd946a08b
DroidOPS <90950640+DroidOPS@users.noreply.github.com>
2024-03-28 07:49:55 +0530

    Update connectDB.js

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-    host: '65.109.122.227',
+    host: '37.27.108.55',
     user: 'fcppyaam_win100nuser',
     password: 'Win100@P4radoxe$2024',
     database: 'fcppyaam_win100new'
